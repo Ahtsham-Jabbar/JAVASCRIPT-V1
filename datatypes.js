01e945c7be9624af2927237bf697ab29b7707836
Ahtsham Jabbar <jattkashi702@gmail.com>
2024-05-16 10:53:10 +0500

    new changes

---
+++ datatypes.js
@@ -0,0 +1,12 @@
+const accountId = 12455
+let accountName = "Ahtsham"
+var accountEmail = "Sample@gmail.com"
+accountCity = "Lahore"
+let accountState;
+
+// accountId = 67890
+accountName = "Farman"
+accountEmail = "farman@gmail.com"
+accountCity = "sahiwal"
+
+console.table([accountId, accountName, accountEmail, accountCity, accountState])
