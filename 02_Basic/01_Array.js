5c93adfbe9c4349a11b17e9d23fdce9f0837a5de
Ahtsham Jabbar <jattkashi702@gmail.com>
2024-05-21 11:58:00 +0500

    Create New Folders

---
+++ 02_Basic/01_Array.js
@@ -0,0 +1,13 @@
+// Array
+
+const myArray = [3, 5, 7, 9, 1]
+const HeroArray = ["Shaktiman", "Spiderman", "IronMan"]
+console.log("A", myArray);
+// console.log(HeroArray);
+
+// myArray.push(5, 2)
+// myArray.pop(2)
+myArray.unshift(5, 6, 7, 8)
+myArray.shift()
+console.log("B", myArray);
+
