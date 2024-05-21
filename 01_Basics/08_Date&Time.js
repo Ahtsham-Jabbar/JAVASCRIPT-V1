470c35cafa8715babbf804d31ecd7489fd641610
Ahtsham Jabbar <jattkashi702@gmail.com>
2024-05-21 11:15:03 +0500

    Change in Basic 1

--- 01_Basics/08_Date&Time.js
+++ 01_Basics/08_Date&Time.js
@@ -1,13 +1,28 @@
 // Date 
 
 let date = new Date()
-console.log(date);
-console.log(date.toString());
-console.log(date.toDateString());
-console.log(date.toLocaleString());
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toLocaleString());
 
-console.log(typeof (date));
+// console.log(typeof (date));
 
-let newDate = new Date(2023, 0, 23)
-console.log(newDate);
-console.log(newDate.toDateString());
+// let newDate = new Date(2023, 0, 23)
+// let newDate = new Date(2023, 0, 23, 5, 6)
+// let newDate = new Date("2023-01-24")
+let newDate = new Date("01-24=2023")
+// console.log(newDate);
+// console.log(newDate.toDateString());
+// console.log(newDate.toLocaleString());
+
+let myNewDate = Date.now()
+// console.log(myNewDate);
+// console.log(newDate.getTime());
+// console.log(Date.now() / 1000);
+// console.log(Math.round(Date.now() / 1000));
+
+let myDate = new Date()
+// console.log(myDate.getDate());
+// console.log(myDate.getMonth() + 1);
+// console.log(myDate.getFullYear());
