55dd6f6d4a972536cef4513021dd0f6125a47f6e
Ahtsham Jabbar <jattkashi702@gmail.com>
2024-05-21 23:09:28 +0500

    Write Code

--- 02_Basic/01_Array.js
+++ 02_Basic/01_Array.js
@@ -2,12 +2,22 @@
 
 const myArray = [3, 5, 7, 9, 1]
 const HeroArray = ["Shaktiman", "Spiderman", "IronMan"]
-console.log("A", myArray);
+// console.log("A", myArray);
 // console.log(HeroArray);
 
 // myArray.push(5, 2)
 // myArray.pop(2)
-myArray.unshift(5, 6, 7, 8)
-myArray.shift()
-console.log("B", myArray);
+// myArray.unshift(8,3)
+// myArray.shift()
+// console.log("B", myArray);
+// console.log(myArray.includes(4))
+// console.log(myArray.indexOf(3))
 
+// const newArray = myArray.join()
+// console.log(newArray);
+const SpliceArray = [2, 3, 4, 5, 6, 7, 8]
+console.log("A", SpliceArray);
+console.log(SpliceArray.slice(1, 3));
+console.log("B", SpliceArray);
+console.log(SpliceArray.splice(1, 3));
+console.log("C", SpliceArray);
